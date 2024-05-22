212a9f1f64688e4f06aca60e971365507627ea37
knox26 <bvjadhav7558@gmail.com>
2024-05-22 22:01:55 +0530

    modified:   pages/5_News.py

--- pages/5_News.py
+++ pages/5_News.py
@@ -26,7 +26,7 @@ scraped_articles = []
 
 def reduce_paragraph(paragraph, num_sentences=2):
     """Reduces a paragraph to a specified number of sentences using LexRank summarization."""
-    parser = PlaintextParser.from_string(paragraph, tokenizer("english"))
+    parser = PlaintextParser.from_string(paragraph, tokenizer.tokenize)
     summarizer = LexRankSummarizer()
     summary = summarizer(parser.document, num_sentences)
     reduced_paragraph = ' '.join(str(sentence) for sentence in summary)
