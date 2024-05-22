49de581bafb7793acbba89a362d0787e952592ec
knox26 <bvjadhav7558@gmail.com>
2024-05-22 17:38:21 +0530

    modified:   pages/5_News.py

--- pages/5_News.py
+++ pages/5_News.py
@@ -12,8 +12,8 @@ st.set_page_config(
         layout="wide",
         initial_sidebar_state="expanded",
 )
-
-tokenizer = nltk.data.load('punkt/english.pickle')
+tokenpath= os.path.join('pages', 'punkt', 'english.pickle')
+tokenizer = nltk.data.load(tokenpath)
 
 with open(os.path.join('pages', 'styles2.css')) as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True) 
